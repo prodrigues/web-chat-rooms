12e41c1520c4b75d7b5446e80d1fc806d0605313
Paulo Renato Dias Rodrigues <Paulo@Paulos-MacBook-Pro.local>
2016-11-11 16:23:14 +0000

    FIX: Update configuration of the api dispatcher servlet

--- web-chat-rooms-webapp-api/src/main/java/com/chatrooms/api/controllers/IndexController.java
+++ web-chat-rooms-webapp-api/src/main/java/com/chatrooms/api/controllers/IndexController.java
@@ -3,11 +3,13 @@ package com.chatrooms.api.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
 
 	@RequestMapping(method = RequestMethod.GET, value="/")
+	@ResponseBody
 	public String index() {
 		return "It's working Jim!";
 	}
